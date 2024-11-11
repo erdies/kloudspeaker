9b6c372206a871586ce59d538976c33b1f0951ff
Martin Erdtmann <forum@erdie.de>
2024-11-11 20:05:10 +0100

    test commit

--- src/main.cpp
+++ src/main.cpp
@@ -18,7 +18,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-// application header // test
+// application header
 #include "kloudspeakerwindow.h"
 #include "kloudspeakerdebug.h"
 
