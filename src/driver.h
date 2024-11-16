42249c07d3ec639f63dd5d6d6ec8fa1f0dc54cbb
Martin Erdtmann <forum@erdie.de>
2024-11-16 12:46:52 +0100

    initialize kloudspeaker on Erdies repo

--- src/driver.h
+++ src/driver.h
@@ -1,20 +1,3 @@
-/***************************************************************************
-                          driver.h  -  description
-                             -------------------
-    begin                : Son Jul 28 2002
-    copyright            : (C) 2002 by Martin Erdtmann
-    email                : martin.erdtmann@gmx.de
- ***************************************************************************/
-
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
-
 #ifndef DRIVER_H
 #define DRIVER_H
 
